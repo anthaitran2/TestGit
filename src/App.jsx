772c54358a1700ba76cc1bebce5795a2690e4b00
iamAnThaiTran <anthaitran1001@gmail.com>
2025-07-09 21:34:53 +0700

    branch 1

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className="card2">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -29,7 +29,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
 
-      <p>an yeu thu phuong</p>
+      <p>an yeu thu phuong vc1</p>
     </>
   );
 }
